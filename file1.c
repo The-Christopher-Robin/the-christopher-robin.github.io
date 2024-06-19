e9fa6ff81936534efaa054e10a5a9d61067e860e
The-Christopher-Robin <68804352+The-Christopher-Robin@users.noreply.github.com>
2024-06-19 07:21:49 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -383,26 +383,22 @@ return 0;}
 #include<unistd.h>
 #include<fcntl.h>
 #include<sys/wait.h>
-int main(){
-int pid = fork();
-if(pid<0){
-printf("error");
-}
-else if(pid == 0){
-printf("child process execution started ids are (PID: %d, PPID: %d)\n",getpid(),
-getppid());
-const char * fp = "file.txt";
-if(access(fp,F_OK)==0){
-printf("child process can access file1.txt");
-}
-if (access(fp, R_OK) == 0) {
-printf("Child Process: File %s is readable.\n", fp);}
-}
-else{
-wait(NULL);
-printf("Parent Process (PID: %d): Child process (PID: %d) exited.\n", getpid(), pid);
-}
-return 0;
+int main() {
+    int pid = fork();
+
+    if (pid == 0) { 
+        printf("Child process execution started. IDs are (PID: %d, PPID: %d)\n", getpid(), getppid());
+
+        const char *fp = "file.txt";
+            printf("Child process can access file1.txt\n");
+            printf("Child Process: File %s is readable.\n", fp);
+        
+    } else {
+        wait(NULL);
+        printf("Parent Process (PID: %d): Child process (PID: %d) exited.\n", getpid(), pid);
+    }
+
+    return 0;
 }
 
 CODE2/dest.txt
