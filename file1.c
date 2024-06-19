956e4719fa044c7d40a952a4c09f9962b93578c0
The-Christopher-Robin <68804352+The-Christopher-Robin@users.noreply.github.com>
2024-06-19 06:00:14 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -270,11 +270,11 @@ while (1) {
 #include<dirent.h>
 #include<sys/stat.h>
 int main(){
-DIR *dir;
+DIR *dir = opendir(".");
 struct dirent *entry;
 struct stat fs;
-dir opendir(".");
-while ((entry=readdir(dir))!=NULL) {
+
+while (entry=readdir(dir)) {
 stat (entry->d_name,&fs);
 if (fs.st_size==0){
 printf("empty files are: %s", entry->d_name);
