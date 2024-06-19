c2cee6a7d2b6f399a35cc6e02c189838ffff35d8
The-Christopher-Robin <68804352+The-Christopher-Robin@users.noreply.github.com>
2024-06-19 05:56:52 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -97,14 +97,14 @@ return 0;
 int main(){
 	struct dirent *dir;
 	struct stat mystat;
-	DIR *dp;
-	dp = opendir(".");
+	DIR *dp = opendir(".");
 	if(dp){
 		while(dir = readdir(dp)){
 			stat(dir->d_name,&mystat);
 			printf("%ld %o %d %d %s %s\n",mystat.st_ino,mystat.st_mode,mystat.st_uid,mystat.st_gid,ctime(&mystat.st_atime),dir->d_name);
 		}
 	}
+	return 0;
 }
 
 4a
