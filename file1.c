f474351560ae791b9ccf82d3092751f57c4334fe
The-Christopher-Robin <68804352+The-Christopher-Robin@users.noreply.github.com>
2024-07-16 08:54:56 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -395,27 +395,53 @@ remove(entry->d_name);
 return 0;}
 
 8b
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 int main() {
-    int pid = fork();
-
-    if (pid == 0) { 
-        printf("Child process execution started. IDs are (PID: %d, PPID: %d)\n", getpid(), getppid());
-
-        const char *fp = "file.txt";
-            printf("Child process can access file1.txt\n");
-            printf("Child Process: File %s is readable.\n", fp);
-        
-    } else {
-        wait(NULL);
-        printf("Parent Process (PID: %d): Child process (PID: %d) exited.\n", getpid(), pid);
-    }
-
-    return 0;
+ pid_t pid;
+ int status;
+ pid_t parent_pid = getpid();
+ pid = fork();
+ if (pid == -1) {
+ perror("fork");
+ exit(EXIT_FAILURE);
+ } else if (pid == 0) {
+ pid_t child_pid = getpid();
+ printf("Child process (PID: %d) executing...\n", child_pid);
+ execl("./p1", "p1", "test.txt", (char *)NULL);
+ perror("execl");
+ exit(EXIT_FAILURE);
+ } else {
+ printf("Parent process (PID: %d) executing...\n", parent_pid);
+ waitpid(pid, &status, 0);
+ printf("Parent process: Child process (PID: %d) has exited.\n", pid);
+ }
+ return 0;
 }
+
+p1
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+ if (argc != 2) {
+ fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+ exit(EXIT_FAILURE);
+ }
+ char *filename = argv[1];
+ if (access(filename, F_OK) == 0) {
+ printf("File '%s' exists and can be accessed.\n", filename);
+ } else {
+ printf("File '%s' does not exist or cannot be accessed.\n", filename);
+ }
+ return 0;
+}
+
+
 
 CODE2/dest.txt
 kri
